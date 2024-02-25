e9ac25c4158d80a6eb553aee682934d1bea0b375
Ankit <ankit27866@gmail.com>
2024-02-25 17:27:16 +0530

    Code Done Res Pending

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./Quote.css";
+import { ColorRing } from "react-loader-spinner";
 
 const Quote = () => {
   const [randomQuote, setRandomeQuote] = useState("");
@@ -28,10 +29,24 @@ const Quote = () => {
       <div className="row-container">
         <div className="quote-div">
           <h1 className="landing-heading">Random Quotes</h1>
-          <p className="para">{randomQuote}</p>
-          <button onClick={generateRandomeQuote} className="btn btn-dark">
-            Get Quote
-          </button>
+          {loading ? (
+            <ColorRing
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="color-ring-loading"
+              wrapperStyle={{}}
+              wrapperClass="color-ring-wrapper"
+              colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+            />
+          ) : (
+            <>
+              <p className="para">{randomQuote}</p>
+              <button onClick={generateRandomeQuote} className="btn btn-dark">
+                Get Quote
+              </button>
+            </>
+          )}
         </div>
       </div>
     </section>
